652f9263eeb0ee510ae02bd6cb57483a5248f39f
Mises Dev <misesdev@onionmail.org>
2025-08-19 02:08:07 -0300

    more one fixing on esplora service

--- rpc-proxy/index.ts
+++ rpc-proxy/index.ts
@@ -7,22 +7,27 @@ import axios from "axios"
 const app = express();
 app.use(bodyParser.json());
 const appPort = process.env.RPC_PROXY_PORT
+const authToken = process.env.RPC_ACCESS_TOKEN
 
 app.post("/:network", async (req, res) => {
     
     const token = req.headers["authorization"];
-    if (token !== `Bearer ${process.env.RPC_ACCESS_TOKEN}`) 
-        return res.status(401).json({ error: "Unauthorized" });
+    if (token !== `Bearer ${authToken}`) { 
+        console.log(`Unauthorized access token ${token} with ${authToken}`)
+        return res.status(401).json({ error: "Unauthorized" })
+    }
 
     const { network } = req.params;
     const bitcoinNode = Nodes[network as "mainnet"|"testnet"]
-    if (!bitcoinNode) 
-        return res.status(400).json({ error: "Invalid network" });
+    if (!bitcoinNode) { 
+        console.log(`Unspected network ${network}`)
+        return res.status(400).json({ error: "Invalid network" })
+    }
 
     const { method, params } = req.body;
 
-    if (!AllowedMethods.includes(method)) 
-        return res.status(403).json({ error: "Method not allowed" });
+    // if (!AllowedMethods.includes(method)) 
+    //     return res.status(403).json({ error: "Method not allowed" });
 
     try 
     {
@@ -37,15 +42,15 @@ app.post("/:network", async (req, res) => {
             id: "proxy",
             method,
             params
-        });
-        res.json(response.data);
+        })
+        res.json(response.data)
     } 
     catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal error" });
+        console.error(err)
+        res.status(500).json({ error: "Internal error" })
     }
 });
 
 app.listen(appPort, () => {
-    console.log(`RPC Proxy server running on port ${appPort}`);
+    console.log(`RPC Proxy server running on port ${appPort}`)
 })
